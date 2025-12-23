fdf640feb64318eb4f672f5cda466913c8e02db8
Lewin Xander <lewinxander@gmail.com>
2025-12-23 15:44:46 +0700

    feat: restructure MainLayout to include header and improve layout organization

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -4,7 +4,16 @@ function MainLayout() {
             <AppSidebar />
 
             <SidebarInset>
-                <Outlet />
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2" />
+                    </div>
+                </header>
+
+                <div className="p-4 pt-0">
+                    <Outlet />
+                </div>
             </SidebarInset>
         </SidebarProvider>
     );
